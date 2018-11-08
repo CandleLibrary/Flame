3dfdb3d8d06ebd3538673cb8c5059d92bb299d43
Anthony Weathersby <anthonycweathersby@gmail.com>
2018-11-08 07:43:28 -0700

    Added Proxy to emit object in scriptIOs
    WIP - Updating SVG code

--- assets/build/flame.node.js
+++ assets/build/flame.node.js
@@ -2063,126 +2063,142 @@ class colorPicker {
 	}
 }
 
-const paper  = require("paper");
+const paper = require("paper");
 const Point = paper.Point;
 const Size = paper.Size;
+const Path = paper.Path;
 
 /**
  * @brief Provides interface tools for manipulating SVG elements
  */
-class SVGManager{
-	constructor(system){
-		this.system =  system;
+class SVGManager {
+    constructor(system) {
+        this.system = system;
 
-		this.target = null;
+        this.target = null;
 
-		this.canvas = document.createElement("canvas");
-		this.canvas.style.position = "absolute";
-		paper.setup(this.canvas);
-		this.proj = paper.project;
-		let point = new Point(0, 0);
+        this.canvas = document.createElement("canvas");
+        this.canvas.style.position = "absolute";
+        paper.setup(this.canvas);
+        this.proj = paper.project;
+        let point = new Point(0, 0);
 
-		this.selection = null;
+        this.selection = null;
 
-		let dx = 0;
-		let dy = 0;
-		let POINTER_DOWN = false;
-
-		this.canvas.addEventListener("pointerdown",(e)=>{
-			let x = e.offsetX;
-			let y = e.offsetY;
-			dx = x;
-			dy = y;
-			point.x = x;
-			point.y = y;
-
-			POINTER_DOWN = true;
-
-			console.log(x,y);
-			
-			this.selection = this.proj.hitTest(point,
-				{fill:true, stroke:true}
-			);
-
-			console.log(this.selection);
-
-			if(this.selection){
-				this.selection.item.selected = true;
-				this.proj.view.update();
-			}
-
-		});
-
-		this.canvas.addEventListener("pointermove", (e)=>{
-			if(!POINTER_DOWN) return;
-			let x = dx - e.offsetX;
-			let y = dy - e.offsetY;
-			console.log(x,y, this.selection);
-			dx = e.offsetX;
-			dy = e.offsetY;
-			let selection = this.selection;
-			if(selection){
-				let item = selection.item;
-				switch(selection.type){
-					case "fill":
-					case "stroke":
-						item.translate(new Point(-x,-y));
-					break;
-				}
-
-				this.proj.view.update();
-			}
-		});
-
-		this.canvas.addEventListener("pointerup", (e)=>{
-			POINTER_DOWN = false;
-			this.export();
-		});
+        let dx = 0;
+        let dy = 0;
+        let POINTER_DOWN = false;
+        let path$$1;
 
 
-		this.ctx = this.canvas.getContext("2d");
-		this.elements = [];
-	}
+        this.canvas.addEventListener("pointerdown", (e) => {
+            let x = e.offsetX - 20;
+            let y = e.offsetY - 20;
+            dx = x;
+            dy = y;
+            point.x = x;
+            point.y = y;
 
-	export(){
-		paper.project.view.viewSize.set(this.width, this.height);
-		paper.project.view.translate(new Point(-20, -20));
-		let output = paper.project.exportSVG({asString:true});
-		console.log(output);
-		this.wick_node.reparse(output).then(n=>this.wick_node = n);
-		paper.project.view.translate(new Point(20, 20));
-		paper.project.view.viewSize.set(this.width+40, this.height+40);
-	}
-
-	mount(ui, target_element, component, x,y){
-
-
-		while(target_element && target_element.tagName.toUpperCase() !== "SVG"){
-			target_element = target_element.parentElement;
-		}
-
-		if(!target_element) return;
-
-		this.wick_node = target_element.wick_node;
-
-		//parse svg elements and build objects from them. 
-		let children = target_element.children;
+            if (e.button == 0) {
+                if (!path$$1) {
+                    path$$1 = new Path();
+                    path$$1.strokeColor = "black";
+                    path$$1.fullySelected = true;
+                } else {
+                    path$$1.add(point);
+                }
+            }else{
+            	path$$1.closePath();
+            }
+            this.proj.view.update();
 
 
-		let rect = target_element.getBoundingClientRect();
-		x = component.x + rect.x + 4 - 20;
-		y = component.y + rect.y + 4 - 20;
-		this.width = rect.width;
-		this.height = rect.width;
-		paper.project.view.viewSize.set(rect.width + 40, rect.height + 40);
-		paper.project.view.translate(new Point(20, 20));
-		paper.project.importSVG(target_element.outerHTML);
+            return;
+            POINTER_DOWN = true;
 
-		this.canvas.style.left = `${x}px`;
-		this.canvas.style.top = `${y}px`;
+            console.log(x, y);
 
-		ui.view_element.appendChild(this.canvas);
-	}
+            this.selection = this.proj.hitTest(point, { fill: true, stroke: true });
+
+            console.log(this.selection);
+
+            if (this.selection) {
+                this.selection.item.selected = true;
+                this.proj.view.update();
+            }
+
+        });
+
+        this.canvas.addEventListener("pointermove", (e) => {
+            if (!POINTER_DOWN) return;
+            let x = dx - e.offsetX;
+            let y = dy - e.offsetY;
+            console.log(x, y, this.selection);
+            dx = e.offsetX;
+            dy = e.offsetY;
+            let selection = this.selection;
+            if (selection) {
+                let item = selection.item;
+                switch (selection.type) {
+                    case "fill":
+                    case "stroke":
+                        item.translate(new Point(-x, -y));
+                        break;
+                }
+
+                this.proj.view.update();
+            }
+        });
+
+        this.canvas.addEventListener("pointerup", (e) => {
+            POINTER_DOWN = false;
+            this.export();
+        });
+
+
+        this.ctx = this.canvas.getContext("2d");
+        this.elements = [];
+    }
+
+    export () {
+        paper.project.view.viewSize.set(this.width, this.height);
+        paper.project.view.translate(new Point(-20, -20));
+        let output = paper.project.exportSVG({ asString: true });
+        console.log(output);
+        this.wick_node.reparse(output).then(n => this.wick_node = n);
+        paper.project.view.translate(new Point(20, 20));
+        paper.project.view.viewSize.set(this.width + 40, this.height + 40);
+    }
+
+    mount(ui, target_element, component, x, y) {
+
+
+        while (target_element && target_element.tagName.toUpperCase() !== "SVG") {
+            target_element = target_element.parentElement;
+        }
+
+        if (!target_element) return;
+
+        this.wick_node = target_element.wick_node;
+
+        //parse svg elements and build objects from them. 
+        let children = target_element.children;
+
+
+        let rect = target_element.getBoundingClientRect();
+        x = component.x + rect.x + 4 - 20;
+        y = component.y + rect.y + 4 - 20;
+        this.width = rect.width;
+        this.height = rect.width;
+        paper.project.view.viewSize.set(rect.width + 40, rect.height + 40);
+        paper.project.view.translate(new Point(20, 20));
+        paper.project.importSVG(target_element.outerHTML);
+
+        this.canvas.style.left = `${x}px`;
+        this.canvas.style.top = `${y}px`;
+
+        ui.view_element.appendChild(this.canvas);
+    }
 }
 
 const pi2 = Math.PI * 2;

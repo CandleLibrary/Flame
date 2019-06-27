a4906de17d98cf7df6a4f437d2ca355f08830b04
galactrax <anthonycweathersby@gmail.com>
2019-06-27 07:57:31 -0600

    removed assets folder

--- assets/misc/service_worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  console.log("INSTALLED!")
-});
-
-self.addEventListener('fetch', function(event) {
-    return fetch(event.request);
-});
--- assets/test/source.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(chia, app, client, flame){
-	describe("Wick source based actions", function(){
-		it.skip("Allows source components to terminate at the iFrame component.", function(){})
-		it.skip("Allows elements that have touch/pointer events to be selected for flame manipulation", function(){})
-	})
-} 

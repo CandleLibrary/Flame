620ba894fed229cca98730d71d85cade1795fe6f
Anthony Weathersby <anthonycweathersby@gmail.com>
2020-08-08 15:24:01 -0600

    Modify editor components

--- source/editor/components/editor.jsx
+++ source/editor/components/editor.jsx
@@ -2,6 +2,7 @@ import adjust_box from "./element_adjust_box.jsx";
 import add_box from "./element_add_box.jsx";
 import hover_box from "./element_hover_box.jsx";
 import border_box from "./border_adjust_box.jsx";
+import color_box from "./color_adjust_box.jsx";
 import { POINTER_DN } from "@model:flame-editor";
 
 <style>
@@ -26,5 +27,5 @@ import { POINTER_DN } from "@model:flame-editor";
 export default <div>
     <hover_box />
     <adjust_box />
-    <border_box />
+    <color_box />
 </div>;
